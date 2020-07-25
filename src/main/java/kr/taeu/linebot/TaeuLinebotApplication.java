b2c7cc5029fa914732d6791802e7b7fbe76f2505
Taewoo Lee <lteawoo@naver.com>
2020-07-25 15:15:31 +0900

    init

---
+++ src/main/java/kr/taeu/linebot/TaeuLinebotApplication.java
@@ -0,0 +1,13 @@
+package kr.taeu.linebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaeuLinebotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaeuLinebotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/taeu/linebot/TaeuLinebotApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.taeu.linebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaeuLinebotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
