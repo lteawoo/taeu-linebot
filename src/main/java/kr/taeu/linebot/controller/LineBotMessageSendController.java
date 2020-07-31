c75741be8b49dc0bb456b9e8ae5633ae5c649141
이태우 <twlee@foodtechkorea.com>
2020-07-31 17:06:37 +0900

    rename class

--- src/main/java/kr/taeu/linebot/controller/LineBotMessageSendController.java
+++ src/main/java/kr/taeu/linebot/controller/LineBotMessageSendController.java
@@ -15,7 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-public class BotController {
+public class LineBotMessageSendController {
   private final LineMessageService lineMessageService;
   
   @PostMapping("/callapi")
